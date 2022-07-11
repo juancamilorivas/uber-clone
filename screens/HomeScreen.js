d91357a7d37ed64d33b65167eb77cc54235c89d3
juancamilorivas <bogota.diamond@hotmail.com>
2022-07-10 21:56:02 -0500

    image logo added, everything looks good till minute 54:12

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import tw from "twrnc";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
