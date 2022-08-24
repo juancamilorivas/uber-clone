208ef25288888ae8475da4dd4f568030ed664e32
juancamilorivas <bogota.diamond@hotmail.com>
2022-08-24 02:11:33 -0500

    Everything looks good till min 2:08:09, navSlice was modify because i forgot to chapnge some values

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,15 @@
 import React from "react";
-import {  Text, View, SafeAreaView, Image } from "react-native";
+import { Text, View, SafeAreaView, Image } from "react-native";
 import tw from "twrnc";
 import NavOptions from "../components/NavOptions";
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import {GOOGLE_MAPS_APIKEY} from "@env";
-
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -21,9 +24,41 @@ const HomeScreen = () => {
           }}
         />
 
-       
+        <GooglePlacesAutocomplete
+          placeholder="Where from?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
 
-        <NavOptions/>
+          onPress={(data, details = null) => {
+
+            dispatch(
+              setOrigin({
+                location: details?.geometry?.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+
+          fetchDetails={true}
+          returnKeyType={"Search"}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
+
+        <NavOptions />
       </View>
     </SafeAreaView>
   );
--- slices/navSlice.js
+++ slices/navSlice.js
@@ -1,52 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-//REDUX:
-//INITIAL STATE
-//REDUCERS(SLICE)
-//EXPORT REDUCERS "actions"
-//SELECTORS
-//EXPORT SLICE "reducer"
-
-
-//INITIAL STATE****************************************************************
-//siempre debe haber un estado inicial que por convencion se llama "initialState"
 const initialState = {
   origin: null,
   destination: null,
   travelTimeInformation: null,
 };
 
-//REDUCERS********************************************************************
-//con los reducer manipulamos la informacion o la "data" que tiene el "initialState"
 export const navSlice = createSlice({
   name: "nav",
   initialState,
-  reducer: {
+  reducers: {
     setOrigin: (state, action) => {
       state.origin = action.payload;
     },
     setDestination: (state, action) => {
-      state.origin = action.payload;
+      state.destination = action.payload;
     },
     setTravelTimeInformation: (state, action) => {
-      state.origin = action.payload;
+      state.travelTimeInformation = action.payload;
     },
   },
 });
 
-//EXPORT REDUCERS************************************************************
-//Esta es la exportacion de los "actions" que se usan en el "reducer"
-export const { setOrigin, setDestination, setTravelTimeInformation } =
-  navSlice.actions;
 
-//SELECTORS**************************************************************************
-// Los selectores son los que envian de nuevo la informacion despues de que los reducer han hecho sus "Actions"
+export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions;
+
 export const selectOrigin = (state) => state.nav.origin;
 export const selectDestination = (state) => state.nav.destination;
-export const selectTravelTimeInformation = (state) =>
-  state.nav.travelTimeInformation;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
 
-
-  //EXPORT SLICE**************************************************************************
-  //Esta es la exportacion del "slice" con el reducer
 export default navSlice.reducer;
