8d36b4f6a706e68e62bd3a613433906af8fa4ffe
juancamilorivas <bogota.diamond@hotmail.com>
2022-09-01 00:48:05 -0500

    Just reporting a bug that was solved at Map.js, the null on the useReff were missing

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { useSelector } from "react-redux";
@@ -6,24 +6,35 @@ import tw from "twrnc";
 import { selectDestination, selectOrigin } from "../slices/navSlice";
 import MapViewDirections from "react-native-maps-directions";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import {useRef} from 'react'
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+    //zoom & fit to markers
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+    });
+  }, [origin, destination]);
 
   return (
     <MapView
+      ref={mapRef}
       style={tw`flex-1`}
       mapType="mutedStandard"
       initialRegion={{
         latitude: origin.location.lat,
-        longitude: origin.location.lng,
+        longitude: origin.location.lng, 
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       }}
     >
       {origin && destination && (
-          <MapViewDirections
+        <MapViewDirections
           origin={origin.description}
           destination={destination.description}
           apikey={GOOGLE_MAPS_APIKEY}
@@ -43,6 +54,19 @@ const Map = () => {
           identifier="origin"
         />
       )}
+
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title="Destination"
+          description={destination.description}
+          identifier="destination"
+        />
+      )}
+
     </MapView>
   );
 };
