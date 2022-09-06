16866d770aadfbd61598959d7d9e48b64b236d6d
juancamilorivas <bogota.diamond@hotmail.com>
2022-09-06 01:32:13 -0500

    a botton menu was added in MapScreen.js

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -53,7 +53,7 @@ export default function RideOptionsCard() {
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
         <Text style={tw`text-center py-5 text-xl`}>
-          Select a Ride - {travelTimeInformation?.distance.text}
+          Select a Ride - {travelTimeInformation?.distance?.text}
         </Text>
       </View>
 
@@ -77,25 +77,23 @@ export default function RideOptionsCard() {
               source={{ uri: image }}
             />
 
-              {/* title*/}
+            {/* title*/}
             <View style={tw`-ml-6`}>
               <Text style={tw`text-xl font-semibold`}>{title}</Text>
-              <Text>{travelTimeInformation?.duration.text}</Text>
+              <Text>{travelTimeInformation?.duration?.text}</Text>
             </View>
 
-              {/* Price*/}
+            {/* Price*/}
             <Text style={tw`text-xl`}>
               {new Intl.NumberFormat("en-gb", {
                 style: "currency",
                 currency: "GBP",
               }).format(
-                (travelTimeInformation?.duration.value *
+                (travelTimeInformation?.duration?.value *
                   SURGE_CHARGE_RATE *
                   multiplier) /
                   100
               )}
-
-
             </Text>
           </TouchableOpacity>
         )}
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,16 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { View, TouchableOpacity } from "react-native";
 import React from "react";
 import tw from "twrnc";
 import Map from "../components/Map";
 import NavigateCard from "../components/NavigateCard";
 import RideOptionsCard from "../components/RideOptionsCard";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Icon } from "@rneui/themed";
+import { useNavigation } from "@react-navigation/native";
 
 const MapScreen = () => {
   const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
 
   return (
     <View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("HomeScreen")}
+        style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full`}
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
+
       <View style={tw`h-1/2`}>
         <Map />
       </View>
@@ -39,4 +49,3 @@ const MapScreen = () => {
 
 export default MapScreen;
 
-const styles = StyleSheet.create({});
